7521053e022f576bf45f4471bdebd90b13b93c41
JiHyun-Jo7 <141097551+JiHyun-Jo7@users.noreply.github.com>
2023-12-06 10:48:48 +0900

    Assignment Between Variables of Struct

---
+++ 04_assignment.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+struct contact {
+	char name[20];
+	char phone[20];
+	int ringtone;
+};
+
+int main() 
+{
+	struct contact ct = { "Mike", "0101010101", 5 };
+	struct contact ct1 = { "Sam", "0202020202", 1 };
+
+	struct contact ct2 = ct1;
+	printf("ct2 after initailizing with ct1: %s, %s, %d\n", ct2.name, ct2.phone, ct2.ringtone);
+
+	ct2 = ct;
+	printf("ct2 after assignment with ct: %s, %s, %d\n", ct2.name, ct2.phone, ct2.ringtone);
+
+	return 0;
+}
